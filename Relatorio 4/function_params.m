fa6429946b60a64ee59e73c70a8da88f566522b7
Erick <ericknathancoro@hotmail.com>
2025-02-08 20:13:18 -0300

    .

--- Relatorio 4/function_params.m
+++ Relatorio 4/function_params.m
@@ -1,9 +1,14 @@
 load('var.mat')
 load('PI.mat')
+load('PID.mat')
 load('T')
-kpid = pid(C, 'IFormula', 'Backward');
-kp = kpid.kp;
-ki = kpid.ki;
-kd = kpid.kd;
+kpid1 = pid(C, 'IFormula', 'Backward');
+kp1 = kpid1.kp;
+ki1 = kpid1.ki;
+kd1 = kpid1.kd;
+kpid2 = pid(Cpid, 'IFormula', 'Backward');
+kp2 = kpid2.kp;
+ki2 = kpid2.ki;
+kd2 = kpid2.kd;
 umin = 0;
-umax = 14;
+umax = 16;
